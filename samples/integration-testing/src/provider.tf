f9d193bc06689c7d7b1161e8bb67637031f69bd1
sonyahash <85360714+sonyahash@users.noreply.github.com>
2023-04-18 13:49:13 -0500

    Update provider.tf

--- samples/integration-testing/src/provider.tf
+++ samples/integration-testing/src/provider.tf
@@ -1,3 +1,13 @@
+terraform {
+  cloud {
+    organization = "sonya-pieklik"
+
+    workspaces {
+      name = "learn-terraform-azure-pipelines"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
