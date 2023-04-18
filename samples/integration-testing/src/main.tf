779729709f3e5ebf541538994cb2e3a3bfa59797
sonyahash <85360714+sonyahash@users.noreply.github.com>
2023-04-18 13:22:38 -0500

    Update main.tf

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  cloud {
+    organization = "sonya-pieklik"
+
+    workspaces {
+      name = "learn-terraform-azure-pipelines"
+    }
+  }
+}
+
+
 resource "random_uuid" "uuid" {}
 
 resource "azurerm_resource_group" "rg" {
